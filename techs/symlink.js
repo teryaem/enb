3fd2dc1df48fb6be5fd9e1314d52af9939193acd
teryaem <teryaem@gmail.com>
2015-01-28 20:35:56 +0300

    add symlinkNode

--- techs/symlink.js
+++ techs/symlink.js
@@ -9,18 +9,24 @@
  *
  * * *String* **fileTarget** — Исходный таргет. Обязательная опция.
  * * *String* **symlinkTarget** — Результирующий таргет. Обязательная опция.
+ * * *String* **symlinkNode** — Результирующая нода.
  *
  * **Пример**
  *
  * ```javascript
+ * var path = require('path');
+ * var nodeDir = nodeConfig.getNodePath();
+ * var publicDir = path.join(nodeDir, '../../public');
  * nodeConfig.addTech([ require('enb/techs/symlink'), {
  *   fileTarget: '?.css',
- *   symlinkTarget: '_?.css'
+ *   symlinkTarget: '_?.css',
+ *   symlinkNode: publicDir
  * } ]);
  * ```
  */
 var vowFs = require('../lib/fs/async-fs');
 var inherit = require('inherit');
+var path = require('path');
 
 module.exports = inherit(require('../lib/tech/base-tech'), {
     getName: function () {
@@ -28,8 +34,9 @@ module.exports = inherit(require('../lib/tech/base-tech'), {
     },
 
     configure: function () {
-        this._symlinkTarget = this.getRequiredOption('symlinkTarget');
         this._fileTarget = this.getRequiredOption('fileTarget');
+        this._symlinkTarget = this.getRequiredOption('symlinkTarget');
+        this._symlinkNode = this.getOption('symlinkNode');
     },
 
     getTargets: function () {
@@ -37,23 +44,32 @@ module.exports = inherit(require('../lib/tech/base-tech'), {
     },
 
     build: function () {
+        var _this = this;
+        var fileTarget = this.node.unmaskTargetName(this._fileTarget);
+        var fileTargetPath = path.join(this.node.getDir(), fileTarget);
         var symlinkTarget = this.node.unmaskTargetName(this._symlinkTarget);
         var symlinkTargetPath = this.node.resolvePath(symlinkTarget);
-        var fileTarget = this.node.unmaskTargetName(this._fileTarget);
-        var _this = this;
+
+        if (this._symlinkNode) {
+            symlinkTargetPath = path.join(this._symlinkNode, symlinkTarget);
+        }
+
         function createSymlink() {
-            return vowFs.symLink(fileTarget, symlinkTargetPath).then(function () {
+            return vowFs.symLink(fileTargetPath, symlinkTargetPath).then(function () {
                 _this.node.resolveTarget(symlinkTarget);
             });
         }
         return this.node.requireSources([fileTarget]).then(function () {
             return vowFs.exists(symlinkTargetPath).then(function (exists) {
                 if (exists) {
-                    return vowFs.remove(symlinkTargetPath).then(createSymlink);
+                    return false;
+                    // return vowFs.remove(symlinkTargetPath).then(createSymlink);
                 } else {
                     return createSymlink();
                 }
             });
         });
-    }
+    },
+
+    clean: function () {}
 });
